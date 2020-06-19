124ea961140d8b259f27a40790bc916a44c48289
Luis Sukys <luis.sukys@sippulse.com>
2020-06-18 23:06:18 -0300

    resolução do desafio

--- src/main/java/challenge/CriptografiaCesariana.java
+++ src/main/java/challenge/CriptografiaCesariana.java
@@ -1,14 +1,47 @@
 package challenge;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CriptografiaCesariana implements Criptografia {
 
-    @Override
-    public String criptografar(String texto) {
-        throw new UnsupportedOperationException("esse method nao esta implementado aainda");
-    }
+	private static final String ALFABETO = "abcdefghijklmnopqrstuvwxyz";
+	private static final List<String> ALFABETO_LIST = Arrays.asList(ALFABETO.split(""));
+	private static final int KEY = 3;
 
-    @Override
-    public String descriptografar(String texto) {
-        throw new UnsupportedOperationException("esse method nao esta implementado aainda");
-    }
+	@Override
+	public String criptografar(String texto) {
+		return criptografarOuDescriptografar(true, texto);
+	}
+
+	@Override
+	public String descriptografar(String texto) {
+		return criptografarOuDescriptografar(false, texto);
+	}
+
+	private String criptografarOuDescriptografar(boolean isCriptografar, String texto) throws IllegalArgumentException {
+		if (texto == null) {
+			throw new NullPointerException("Text mut not be null.");
+		} else if (texto.isEmpty()) {
+			throw new IllegalArgumentException("Text mut not be empty.");
+		}
+		texto = texto.toLowerCase();
+		List<String> textoAsList = Arrays.asList(texto.split(""));
+		StringBuilder resposta = new StringBuilder();
+		int index = -1;
+		for (String letter : textoAsList) {
+			if (!ALFABETO_LIST.contains(letter)) {
+				resposta.append(letter);
+				continue;
+			}
+			letter = letter.trim().toLowerCase();
+			if (isCriptografar) {
+				index = Math.floorMod(ALFABETO_LIST.indexOf(letter) + KEY, ALFABETO.trim().length());
+			} else {
+				index = Math.floorMod(ALFABETO_LIST.indexOf(letter) - KEY, ALFABETO.trim().length());
+			}
+			resposta.append(ALFABETO_LIST.get(index));
+		}
+		return resposta.toString();
+	}
 }
