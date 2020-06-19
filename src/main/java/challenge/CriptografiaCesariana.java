f404b1e4177a34b218e228a2489bca6d7f92bf3e
Luis Sukys <luis.sukys@sippulse.com>
2020-06-19 10:29:25 -0300

    melhoria do código de acordo com code reviews.

--- src/main/java/challenge/CriptografiaCesariana.java
+++ src/main/java/challenge/CriptografiaCesariana.java
@@ -1,12 +1,10 @@
 package challenge;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class CriptografiaCesariana implements Criptografia {
 
 	private static final String ALFABETO = "abcdefghijklmnopqrstuvwxyz";
-	private static final List<String> ALFABETO_LIST = Arrays.asList(ALFABETO.split(""));
 	private static final int KEY = 3;
 
 	@Override
@@ -19,29 +17,50 @@ public class CriptografiaCesariana implements Criptografia {
 		return criptografarOuDescriptografar(false, texto);
 	}
 
-	private String criptografarOuDescriptografar(boolean isCriptografar, String texto) throws IllegalArgumentException {
-		if (texto == null) {
-			throw new NullPointerException("Text mut not be null.");
-		} else if (texto.isEmpty()) {
-			throw new IllegalArgumentException("Text mut not be empty.");
-		}
+	
+	/**
+	 * Faz a codificação / decodificação do texto informado.
+	 * 
+	 * @param isCriptografar verifica se é para criptografar (true) ou descriptografar (false).
+	 * @param texto Texto que será processado.
+	 * @return retorna o texto criptografado ou descriptografado, de acordo com o que for solicitado.
+	 * 
+	 */
+	private String criptografarOuDescriptografar(boolean isCriptografar, String texto) {
+		this.validarTexto(texto);
 		texto = texto.toLowerCase();
-		List<String> textoAsList = Arrays.asList(texto.split(""));
 		StringBuilder resposta = new StringBuilder();
 		int index = -1;
-		for (String letter : textoAsList) {
-			if (!ALFABETO_LIST.contains(letter)) {
+		for (String letter : Arrays.asList(texto.split(""))) {
+			if (!ALFABETO.contains(letter)) {
 				resposta.append(letter);
 				continue;
 			}
 			letter = letter.trim().toLowerCase();
 			if (isCriptografar) {
-				index = Math.floorMod(ALFABETO_LIST.indexOf(letter) + KEY, ALFABETO.trim().length());
+				index = Math.floorMod(ALFABETO.indexOf(letter) + KEY, ALFABETO.trim().length());
 			} else {
-				index = Math.floorMod(ALFABETO_LIST.indexOf(letter) - KEY, ALFABETO.trim().length());
+				index = Math.floorMod(ALFABETO.indexOf(letter) - KEY, ALFABETO.trim().length());
 			}
-			resposta.append(ALFABETO_LIST.get(index));
+			resposta.append(ALFABETO.charAt(index));
 		}
 		return resposta.toString();
 	}
+	
+	
+	/**
+	 * Verifica se o texto passado é válido.
+	 * @param texto - O texto a ser verificado.
+	 * @throws NullPointerException caso o texto seja nulo.
+	 * @throws IllegalArgumentException caso o texto esteja vazio.
+	 * 
+	 */
+	private void validarTexto(String texto) {
+		if (texto == null) {
+			throw new NullPointerException("Text mut not be null.");
+		} else if (texto.isEmpty()) {
+			throw new IllegalArgumentException("Text mut not be empty.");
+		}
+	}
+	
 }
